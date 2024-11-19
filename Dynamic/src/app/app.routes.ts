fd6669e4842968d0b3d09b1492edd2fea4816728
Naman Singh <naman.singh2402@gmail.com>
2024-11-19 22:15:22 +0530

    update

--- Dynamic/src/app/app.routes.ts
+++ Dynamic/src/app/app.routes.ts
@@ -2,16 +2,10 @@ import { Routes } from '@angular/router';
 import { ContactpageComponent } from './contactpage/contactpage.component';
 import { ContestPageComponent } from './contest-page/contest-page.component';
 import { HomeComponent } from './homepage/home.component';
-import { LayoutComponent } from './layout/layout.component';
 import { LoginSignUpComponent } from './login-sign-up/login-sign-up.component';
 import { RecipesPageComponent } from './recipespage/recipes.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    redirectTo:'login',
-    pathMatch:'full'
-  },
   
   {
     path:'login'
@@ -20,32 +14,25 @@ const routes: Routes = [
   },
   {
     path:''
-    ,component:LayoutComponent
-    ,children:[
-      {
-        path:'home'
-        ,component:HomeComponent
-        ,title:'Home'
-      },
-      {
-        path:'contest',
-        component:ContestPageComponent,
-        title:'Contest'
-      },
-    
-      {
-        path:'contact',
-        component:ContactpageComponent,
-        title:'Contact'
-      }
-      ,{
-        path:'recipes'
-        ,component:RecipesPageComponent
-        ,title:'Recipes'
-      }
-    ]
+    ,component:HomeComponent
+    ,title:'Home'
+  },
+  {
+    path:'contest',
+    component:ContestPageComponent,
+    title:'Contest'
+  },
+
+  {
+    path:'contact',
+    component:ContactpageComponent,
+    title:'Contact'
   }
-  
+  ,{
+    path:'recipes'
+    ,component:RecipesPageComponent
+    ,title:'Recipes'
+  }
   
 ];
 
